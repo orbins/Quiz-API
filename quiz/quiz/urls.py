a0b94d3b8c07a2a3710354e9e92c33a01616d749
Takeshi <orwellj@mail.ru>
2023-05-10 14:41:06 +0300

    fix readme and add basic login url

--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -5,6 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('application.urls', namespace='application')),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
